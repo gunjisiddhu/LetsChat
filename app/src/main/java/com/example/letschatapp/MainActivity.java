4e1e3691f44e7f8dafd9c29e88cd7300af6fcf6e
Siddhu <gunjisiddhu@gmail.com>
2022-04-16 19:59:11 +0530

    Recycler View Pending

--- app/src/main/java/com/example/letschatapp/MainActivity.java
+++ app/src/main/java/com/example/letschatapp/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.letschatapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,26 @@ public class MainActivity extends AppCompatActivity {
 
 
         firebaseAuth = FirebaseAuth.getInstance();
-        //if(firebaseAuth.getCurrentUser()==null)
+        if(firebaseAuth.getCurrentUser()==null){
             startActivity(new Intent(getApplicationContext(),LoginScreen.class));
+        }
+
+        textView = findViewById(R.id.mainmenu_username);
+
+        DatabaseReference data = FirebaseDatabase.getInstance().getReference("Users").child(firebaseAuth.getUid());
+        data.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserData userData = snapshot.getValue(UserData.class);
+                textView.setText(userData.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
